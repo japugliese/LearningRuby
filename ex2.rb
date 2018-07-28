4e8701a404c877805c547268bd6e338023451a2c
japugliese <japugliese@users.noreply.github.com>
2018-07-28 15:37:08 -0700

    Add files via upload

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# anything after the # is ignored by ruby. 
+
+puts "I could have cde like this."
+
+# you can also use a comment to "disable" or comment out a piece of code
+#puts "this won't run"
+
+puts "This will run."
---
+++ ex3.rb
@@ -0,0 +1,28 @@
+#ex3: Numbers and Math
+#Study Drills:
+#1.Above each line, use the # to write a comment to yourself explaining what the line does.
+#2.Remember in Exercise 0 when you started irb? Start irb this way again and, using the math operators, use Ruby as a calculator.
+#3.Find something you need to calculate and write a new .rb file that does it.
+#4.Rewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is floating point.
+
+puts "I will now count my chickens:"
+
+puts "Hens #{25 + 30 / 6}"
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > 2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
